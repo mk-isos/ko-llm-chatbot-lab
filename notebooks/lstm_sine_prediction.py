c6138a052c13b1520d774c1529d1f3843d451b44
Markus <121490196+mk-isos@users.noreply.github.com>
2026-01-27 13:08:18 +0900

    250127 study: predict sine wave sequence using LSTM

---
+++ notebooks/lstm_sine_prediction.py
@@ -0,0 +1,24 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+
+x = np.linspace(0, 100, 1000)
+y = np.sin(x)
+
+n_steps = 10
+X, y_seq = [], []
+for i in range(len(y) - n_steps):
+    X.append(y[i:i + n_steps])
+    y_seq.append(y[i + n_steps])
+
+X, y_seq = np.array(X), np.array(y_seq)
+X = X.reshape((X.shape[0], X.shape[1], 1))
+
+model = Sequential()
+model.add(LSTM(50, activation='relu', input_shape=(n_steps, 1)))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(X, y_seq, epochs=100, verbose=0)
+
+predicted_value = model.predict(X[-1:], verbose=0)
+print(predicted_value[0][0])
